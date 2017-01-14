497480d225ecba0a4637d7efd5f3a78b13daf6e5
Bruno Philipe <brunophilipe@me.com>
2017-01-14 17:05:37 +0100

    Initial Commit

---
+++ TipTyper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TipTyper
+//
+//  Created by Bruno Philipe on 14/1/17.
+//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
