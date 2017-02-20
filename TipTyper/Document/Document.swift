ed0e127a876f2bc3f110939bb5f7c9944f9f02b1
Bruno Philipe <brunophilipe@me.com>
2017-02-20 22:09:39 +0100

    Reorganizes workspace

--- TipTyper/Document/Document.swift
+++ TipTyper/Document/Document.swift
@@ -8,30 +8,36 @@
 
 import Cocoa
 
-class Document: NSDocument {
-
-	override init() {
+class Document: NSDocument
+{
+	override init()
+	{
 	    super.init()
+
 		// Add your subclass-specific initialization here.
 	}
 
-	override class func autosavesInPlace() -> Bool {
+	override class func autosavesInPlace() -> Bool
+	{
 		return true
 	}
 
-	override var windowNibName: String? {
+	override var windowNibName: String?
+	{
 		// Returns the nib file name of the document
 		// If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
 		return "Document"
 	}
 
-	override func data(ofType typeName: String) throws -> Data {
+	override func data(ofType typeName: String) throws -> Data
+	{
 		// Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
 		// You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
 		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
 	}
 
-	override func read(from data: Data, ofType typeName: String) throws {
+	override func read(from data: Data, ofType typeName: String) throws
+	{
 		// Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
 		// You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
 		// If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
